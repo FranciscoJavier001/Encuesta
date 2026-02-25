06aaca906f2135ee4f7929097ccec28ef4dca8ac
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2026-02-24 20:38:26 -0600

    Update

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Encuesta/sw.js', { scope: '/Encuesta/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://FranciscoJavier001.github.io/Encuesta/sw.js', { scope: 'https://FranciscoJavier001.github.io/Encuesta/' })})}
